00fbe795043a998a76b043ce33fbad5552a3ace5
RMoreno207 <107397146+RMoreno207@users.noreply.github.com>
2022-07-23 04:04:23 +0200

    commi a lot of changes

--- js/index.js
+++ js/index.js
@@ -10,17 +10,14 @@ async function getQuestions() {
   const quesElem = document.getElementById("question");
   const answElems = document.querySelectorAll(".btn");
   const allRadio = document.querySelectorAll(".answer");//Obtenemos todos los radio
-  const submitBtn = document.getElementById("submit");
-  // console.log(answElems);
+  const submitBtn = document.getElementById("submit");//Obtenemos el boton siguiente
 
-  console.log(allRadio[0].checked);
   //Añadida función para deseleccionar los radio
   function unselect() {
     allRadio.forEach((x) => x.checked = false);
   }
 
 
-  console.log(allRadio[0].checked);
   let allquestions = await fetch(
     "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
   );
@@ -28,7 +25,6 @@ async function getQuestions() {
 
   function paint(num) {
     let allAnswers = [...infoquestions.results[num].incorrect_answers, infoquestions.results[num].correct_answer]
-    // Hacer que las respuestas salgan en orden aleatorio
     let question = infoquestions.results[num].question
     answerbtn1.innerHTML = allAnswers[0]
     answerbtn2.innerHTML = allAnswers[1]
@@ -62,9 +58,10 @@ async function getQuestions() {
         quiz.innerHTML = `  <div class="quizCont">                        
                            <h2 id="question">You replied ${score}/ 10 correct questions. Keep trying </h2>        
                            <button id="reload" class="reload" onclick="location.reload()">Volver a jugar</button>
+                           <button id="logout" class="reload" onclick="location.reload()">Cerrar sesión</button>
                            </div>
                            `;
-
+        document.getElementById("logout").addEventListener("click", signOut);
 
       }
       currentQuestion += 1;
@@ -72,3 +69,4 @@ async function getQuestions() {
       paint(currentQuestion);
     });
 } getQuestions()
+
---
+++ js/login.js
@@ -0,0 +1,107 @@
+// Your web app's Firebase configuration
+
+//Esta es mi configuracion de database
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjyoE95U6SnQkG0-E3hDDnnEAn1L6G91A",
+  authDomain: "tortillaback.firebaseapp.com",
+  projectId: "tortillaback",
+  storageBucket: "tortillaback.appspot.com",
+  messagingSenderId: "198340461383",
+  appId: "1:198340461383:web:4fb86d3743eb637e62c262"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();// db representa mi BBDD
+
+
+//Crear usuario
+const createUser = (user) => {
+  db.collection("users")
+    .add(user)
+    .then((docRef) => console.log("Document written with ID: ", docRef.id))
+    .catch((error) => console.error("Error adding document: ", error));
+};
+
+//Dar de alta usuario
+const signUpUser = (email, password) => {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      let user = userCredential.user;
+      console.log(`Su usuario ${user.email} se ha registrdo correctamente`)
+      alert(`se ha registrado ${user.email} ID:${user.uid}`)
+      // ...
+      // Guarda El usuario en Firestore
+      createUser({
+        id: user.uid,
+        email: user.email,
+      })
+
+
+    })
+    .catch((error) => {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      console.log("Error en el sistema" + error.message);
+    });
+
+};
+
+//Iniciar sesion
+const signInUser = (email, password) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      let user = userCredential.user;
+      console.log(`se ha logado ${user.email} ID:${user.uid}`)
+      alert(`Hola ${user.email}!!`)
+      console.log(user);
+      const cuUser = firebase.auth().currentUser;//Obtenemos el usuario actual
+      console.log(cuUser.email);
+
+      window.location.href = "./quiz.html";
+
+    })
+    .catch((error) => {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      console.log(errorCode)
+      console.log(errorMessage)
+    });
+}
+
+//Cerrar sesion
+const signOut = () => {
+  let user = firebase.auth().currentUser;
+
+  firebase.auth().signOut().then(() => {
+    console.log("Sale del sistema: " + user.user)
+  }).catch((error) => {
+    console.log("hubo un error: " + error);
+  });
+}
+
+
+// //formularios
+document.getElementById("form1").addEventListener("submit", function (event) {
+  event.preventDefault();
+  let email = event.target.elements.email.value;
+  let pass = event.target.elements.pass.value;
+  let pass2 = event.target.elements.pass2.value;
+
+  pass === pass2 ? signUpUser(email, pass) : alert("error password");
+})
+
+document.getElementById("form2").onsubmit = (event) => {
+  event.preventDefault();
+  let email = event.target.elements.email2.value;
+  let pass = event.target.elements.pass3.value;
+  signInUser(email, pass)
+}
+
+//Fin de datos y funciones FIREBASE ---------------------------------------------------------------------
